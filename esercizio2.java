434ce7bc445738df0dc2dd3c9591678528b26a0c
YasmineCharlie <117976439+YasmineCharlie@users.noreply.github.com>
2022-11-11 10:49:59 +0100

    Creazione esercizio1
    Jasmine Rossi 11-11-22

---
+++ esercizio2.java
@@ -0,0 +1,24 @@
+public class esercizio2 {
+    public static void main(String[]args){
+        //DICHIARAZIONE E VALORIZZAZIONE
+        char val1='b';
+        double val2=123.34;
+        double val3=1;
+        Boolean b1=true;
+        Boolean b2=false;
+        double somma;
+        boolean orP;
+
+        somma= val1+val2+val3;
+        somma/=3;
+
+        orP=b1||b2;
+        if(orP==true){
+            System.out.println(orP);
+            System.out.println("EVVIVA");
+        }
+
+        System.out.println(somma);
+
+    }
+}
